539a410445cd557461ac011665646c23c7e7c1cf
Victor Baleeiro <victor.baleeiro@going2.com.br>
2020-02-03 13:27:32 -0300

    resolvido problema 1001 no URI

---
+++ uri-1001/1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int primeiroNumero;
+    int segundoNumero;
+
+    scanf("%d", &primeiroNumero);
+    scanf("%d", &segundoNumero);
+    printf("X = %d\n", primeiroNumero+segundoNumero);
+
+    return 0;
+}
